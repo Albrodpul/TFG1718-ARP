d9212b936f9878f57f19fdce509a47799b80ab4e
Alberto Jesus Rodriguez Pulido <albrodpul@gmail.com>
2017-10-12 16:40:50 +0000

    Fixed Auth0 Callback url

--- public/js/auth0-variables.js
+++ public/js/auth0-variables.js
@@ -1,3 +1,4 @@
 var AUTH0_CLIENT_ID='XZJuYq4_-9ZBYQzhmlQafZC0boAbeAgI'; 
 var AUTH0_DOMAIN='tfg1718-arp.eu.auth0.com'; 
-var AUTH0_CALLBACK_URL='https://tfg1718-arp-albrodpul.c9users.io/callback';
+var host = window.location.host;
+var AUTH0_CALLBACK_URL='https://'+host+'/callback';
