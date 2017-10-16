1610463167f7a1e5ee4b1d6c35d42297a31c1193
Alberto Jesus Rodriguez Pulido <albrodpul@gmail.com>
2017-10-16 17:36:08 +0000

    Styling profile

--- public/app/app.js
+++ public/app/app.js
@@ -32,13 +32,13 @@
         controller: 'ProfileController',
         templateUrl: 'app/profile/profile.html',
         controllerAs: 'vm'
-      })      
+      })
       .state('callback', {
         url: '/callback',
         controller: 'CallbackController',
         templateUrl: 'app/callback/callback.html',
         controllerAs: 'vm'
-      });;
+      });
 
     // Initialization for the angular-auth0 library
     angularAuth0Provider.init({
@@ -47,14 +47,14 @@
       responseType: 'token id_token',
       audience: 'https://' + AUTH0_DOMAIN + '/userinfo',
       redirectUri: AUTH0_CALLBACK_URL,
-      scope: 'openid'
+      scope: 'openid profile'
     });
 
     $urlRouterProvider.otherwise('/');
 
     $locationProvider.hashPrefix('');
 
-    /// Comment out the line below to run the app
+    // Comment out the line below to run the app
     // without HTML5 mode (will use hashes in routes)
     $locationProvider.html5Mode(true);
   }
---
+++ public/js/preloader.js
@@ -0,0 +1,28 @@
+    $(document).ready(function() {
+      $('.collapsible').collapsible({
+        accordion: false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+      });
+    });
+
+    document.addEventListener("DOMContentLoaded", function(){
+	$('.preloader-background').delay(1700).fadeOut('slow');
+	
+	$('.preloader-wrapper')
+		.delay(1700)
+		.fadeOut();
+});
+
+    $('.dropdown-button').dropdown({
+      inDuration: 300,
+      outDuration: 225,
+      constrainWidth: false, // Does not change width of dropdown to that of the activator
+      hover: false, // Activate on hover
+      gutter: 0, // Spacing from edge
+      belowOrigin: true, // Displays dropdown below the button
+      alignment: 'left', // Displays dropdown with edge aligned to the left of button
+      stopPropagation: false // Stops event propagation
+    }
+  );
+
+
+
