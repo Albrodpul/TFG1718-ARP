211bf1cf71627c9db36efb40bba8c19482f3e08c
Alberto Jesus Rodriguez Pulido <albrodpul@gmail.com>
2017-10-25 18:43:40 +0000

    Delete All Component Working

---
+++ public/RESTClient/angular/js/birth-delete-all/birth-delete-all.component.js
@@ -0,0 +1,33 @@
+'use strict'
+
+angular
+    .module("birthDeleteAll")
+    .component("birthDeleteAll", {
+        templateUrl: 'js/birth-delete-all/birth-delete-all.template.html',
+        controller: ["$scope", "$http", function($scope, $http) {
+            console.log("Delete All Controller initialized");
+
+            $scope.$parent.vLimit = 3;
+            var baseURL = '/api/v1/spain-births';
+
+            var refresh = function() {
+                $http
+                    .get(baseURL + '?limit=' + $scope.$parent.vLimit)
+                    .then(function(response) {
+                        $scope.$parent.births = response.data;
+                        $scope.$parent.myValue = false;
+                    });
+            };
+
+
+            $scope.deleteAll = function() {
+                console.log("Deleting births");
+                $http
+                    .delete(baseURL)
+                    .then(function(response) {
+                        refresh();
+
+                    });
+            };
+        }]
+    });
