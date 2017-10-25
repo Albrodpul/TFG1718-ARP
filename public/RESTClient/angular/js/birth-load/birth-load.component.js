a27a236d2e51bfaa1c938d11fa8629ee49dbfbca
Alberto Jesus Rodriguez Pulido <albrodpul@gmail.com>
2017-10-25 18:15:35 +0000

    Load Birth Component

---
+++ public/RESTClient/angular/js/birth-load/birth-load.component.js
@@ -0,0 +1,31 @@
+'use strict'
+
+      angular
+          .module("birthLoad")
+          .component("birthLoad", {
+              templateUrl: 'js/birth-load/birth-load.template.html',
+              controller: ["$scope", "$http", function($scope, $http) {
+                  console.log("Load Controller initialized");
+                  $scope.vLimit = 3;
+                  var baseURL = '/api/v1/spain-births';
+
+                  var refresh = function() {
+                      $http
+                          .get(baseURL + '?limit=' + $scope.vLimit)
+                          .then(function(response) {
+                              $scope.births = response.data;
+                              $scope.myValue = false;
+                          });
+                  };
+                  
+                  
+                    $scope.loadBirth = function() {
+                      $http
+                          .get(baseURL + "/loadInitialData")
+                          .then(function(response) {
+                              refresh();
+                          });
+                  };
+              }
+              ]
+              });
