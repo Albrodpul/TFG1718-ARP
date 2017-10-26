332d48916d5c279a4bada57e3be954b6924e3ef9
Alberto Jesus Rodriguez Pulido <albrodpul@gmail.com>
2017-10-26 15:50:16 +0000

    Delete Component Working

--- public/RESTClient/angular/js/birth-delete/birth-delete.component.js
+++ public/RESTClient/angular/js/birth-delete/birth-delete.component.js
@@ -4,28 +4,17 @@ angular
     .module("birthDelete")
     .component("birthDelete", {
         templateUrl: 'js/birth-delete/birth-delete.template.html',
-        controller: ["$scope", "$http","$location", function($scope, $http, $location) {
+        controller: ["$scope", "$http","$route", function($scope, $http,$route) {
             console.log("Delete Item Controller initialized");
 
-            $scope.$parent.vLimit = 3;
             var baseURL = '/api/v1/spain-births';
-
-            /*var refresh = function() {
-                $http
-                    .get(baseURL + '?limit=' + $scope.$parent.vLimit)
-                    .then(function(response) {
-                        console.log("Refreshing...");
-                        $location.path("/");
-                    });
-            };*/
-
 
             $scope.deleteBirth = function(region, year) {
                 console.log("Deleting birth with " + region + " " + year);
                 $http
                     .delete(baseURL + '/' + region + '/' + year)
                     .then(function(response) {
-                        $scope.$parent.refresh();
+                        $route.reload();
 
                     });
             };
