14073c2efc61c69464d9b8be0fe9baf2e9e16958
Alberto Rodriguez <albrodpul@gmail.com>
2017-11-07 22:44:16 +0100

    More refactor

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,20 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 import { MaterializeModule } from 'angular2-materialize';
 
-
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ROUTES } from './app.routes';
 
+import { AuthService } from './auth/auth.service';
+import { ProfileComponent } from './profile/profile.component';
+import { CallbackComponent } from './callback/callback.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, 
+    HomeComponent,
+    ProfileComponent, 
+    AboutComponent,   
+    CallbackComponent, 
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    MaterializeModule
+    FormsModule,    
+    MaterializeModule,
+    RouterModule.forRoot(ROUTES)
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
