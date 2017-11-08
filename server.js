dc43b56b4885314b9dd7676325b8168cf820a792
Alberto Rodriguez <albrodpul@gmail.com>
2017-11-08 12:23:38 +0100

    Test

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
-const path = require('path');
-const express = require('express');
-const app = express();
-
+var path = require('path');
+var express = require('express');
+var app = express();
+var cors=require("cors");
+var MongoClient = require('mongodb').MongoClient;
+var mdbURL = "mongodb://test:test@ds159344.mlab.com:59344/tfg1718-arp";
+var BASE_API_PATH = "/api/v1/spain-births";
+var db;
+var port = (process.env.PORT || 8080);
 // If an incoming request uses
 // a protocol other than HTTPS,
 // redirect that request to the
@@ -32,4 +37,18 @@ app.get('/*', function(req, res) {
 
 // Start the app by listening on the default
 // Heroku port
-app.listen(process.env.PORT || 8080);
+MongoClient.connect(mdbURL,{native_parser:true},function(err,database){
+  if(err){
+    console.log("Cannot connect to DB: "+err);
+    process.exit(1);
+  }
+  db=database.collection("spain-births");
+  spainBirths.initial(app,db,BASE_API_PATH);
+  app.listen(port, ()=>{
+    console.log("Magic happens on port: " + port);
+});
+});
+
+app.use(bodyParser.json()); //use default json enconding/decoding
+app.use(helmet()); //improve security
+app.use(cors());
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from './../auth/auth.service';
+import { AboutComponent } from './../about/about.component';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(public auth: AuthService) { }
 
   ngOnInit() {
   }
